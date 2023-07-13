102784b17b12e3781fcc9b3ebb94883d9693da87
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-07-14 03:07:04 +0700

    this is other branch where i show data using v-if and v-else-if and v-else

--- weather-app-vue/src/App.vue
+++ weather-app-vue/src/App.vue
@@ -4,7 +4,7 @@ const weather='r'
 </script>
 
 <template>
-   <section class="container mx-auto flex items-center flex-col">
+   <!-- <section class="container mx-auto flex items-center flex-col">
         <h1 class="text-center text-2xl py-10">Logic</h1>
         <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
             <h1 class="text-3xl">{{location}}</h1>
@@ -24,6 +24,23 @@ const weather='r'
             <h2 class="text-2xl">Weather: Thunderstorm</h2>
             <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
           </div>
+        </div>
+    </section> -->
+    <section class="container mx-auto flex items-center flex-col">
+        <h1 class="text-center text-2xl py-10">Logic</h1>
+        <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
+            <h1 class="text-3xl">{{ location }}</h1>
+            
+              <h2 v-if="weather == 's'" class="text-2xl">Weather: Sunny</h2>
+              <h2 v-else-if="weather == 'pc'" class="text-2xl">Weather: Partly Cloudy</h2>
+              <h2 v-else-if="weather == 'r'" class="text-2xl">Weather:Rainy</h2>
+              <h2 v-else class="text-2xl">Weather: Thunderstorm</h2>
+
+            <img v-if="weather == 's'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">   
+            <img v-if="weather == 'pc'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
+            <img v-else-if="weather == 'r'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt=""> 
+            <img v-if="weather == 't'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
+          
         </div>
     </section>
 </template>
