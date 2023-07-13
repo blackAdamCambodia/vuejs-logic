9cae565ba8ed2e7b7c47a10a0622cb7ef9cd72fb
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-07-14 03:32:02 +0700

    groupwish content show depend on v-if condition

--- weather-app-vue/src/App.vue
+++ weather-app-vue/src/App.vue
@@ -4,23 +4,29 @@ const weather='t'
 </script>
 
 <template>
-    <section class="container mx-auto flex items-center flex-col">
+   <section class="container mx-auto flex items-center flex-col">
         <h1 class="text-center text-2xl py-10">Logic</h1>
         <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
-            <h1 class="text-3xl">{{ location }}</h1>
-            
-              <h2 v-if="weather == 's'" class="text-2xl">Weather: Sunny</h2>
-              <h2 v-else-if="weather == 'pc'" class="text-2xl">Weather: Partly Cloudy</h2>
-              <h2 v-else-if="weather == 'r'" class="text-2xl">Weather:Rainy</h2>
-              <h2 v-else class="text-2xl">Weather: Thunderstorm</h2>
-
-            <img v-if="weather == 's'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">   
-            <img v-if="weather == 'pc'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
-            <img v-else-if="weather == 'r'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt=""> 
-            <img v-if="weather == 't'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
-          
+            <h1 class="text-3xl">{{location}}</h1>
+            <template v-if="weather == 's'">
+              <h2 class="text-2xl">Weather: Sunny</h2>
+            <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
+            </template>
+            <template v-if="weather == 'pc'">
+            <h2 class="text-2xl">Weather: Partly Cloudy</h2>
+            <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
+            </template>
+            <template v-if="weather == 'r'">
+            <h2 class="text-2xl">Weather:Rainy</h2>
+            <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">            
+            </template>
+            <template v-if="weather == 't'">
+            <h2 class="text-2xl">Weather: Thunderstorm</h2>
+            <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
+          </template>
         </div>
     </section>
+   
 </template>
 
 <style scoped>
